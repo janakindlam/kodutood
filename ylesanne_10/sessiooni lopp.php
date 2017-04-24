0bc5614cf9c87ea559f7e6d166f306de26bb4dc2
janakindlam <janakindlam@gmail.com>
2017-04-24 03:41:27 +0300

    Delete sessiooni lopp.php

--- ylesanne_10/sessiooni lopp.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $_SESSION = array();
-    if (isset($_COOKIE[session_name()])) {
-        setcookie(session_name(), '', time()-60000, '/');
-    }
-    session_destroy();
-    header("Location: kontroller.php");
-?>
